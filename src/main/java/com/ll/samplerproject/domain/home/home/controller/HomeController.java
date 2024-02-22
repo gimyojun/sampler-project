da506b00d33871fbf0aff745e2b29e338552b27c
gimyojun <gywnsdl5623@gmail.com>
2024-02-23 00:53:32 +0900

    set

--- src/main/java/com/ll/samplerproject/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/samplerproject/domain/home/home/controller/HomeController.java
@@ -1,2 +1,16 @@
-package com.ll.samplerproject.domain.home.home.controller;public class HomeController {
+package com.ll.samplerproject.domain.home.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "hello world";
+    }
+
+
 }
